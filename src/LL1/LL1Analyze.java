303c758dd9ec9383b6036ff9a138f4e1da8216d6
wx <a19858190365@hdu.edu.cn>
2020-06-12 22:59:36 +0800

    新增LL(1)雏形

---
+++ src/LL1/LL1Analyze.java
@@ -0,0 +1,113 @@
+package LL1;
+
+import java.util.Stack;
+
+public class LL1Analyze {
+	String inputStr;
+	Stack<Character> stack;
+	LL1Analyze(String str){
+		inputStr = str;
+		stack = new Stack();
+		stack.push('$');
+		stack.push('E');
+	}
+	public boolean isVT(char str) {
+		PredictiveTable table = new PredictiveTable();
+		for(char s : table.VT) {
+			if(s == str) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public String isInTable(char vn,char vt){
+		PredictiveTable table = new PredictiveTable();
+		int m = 0;
+		int n = 0;
+		
+		for(int i = 0; i < table.VN.length; i ++) {
+			if(vn == table.VN[i]) {
+				m = i;
+				break;
+			}
+		}
+		for(int i = 0; i < table.VT.length; i ++) {
+			if(vt == table.VT[i]) {
+				n = i;
+				break;
+			}	
+		}
+		if(table.predictTable[m][n].length() > 0) {
+			return table.predictTable[m][n];
+		}else {
+			return "false";
+		}
+	}
+	public void analyze(){
+		int ip = 0;
+		char stackTop;
+		char strFirst;
+		while(!stack.empty()) {
+			stackTop = stack.peek();
+			System.out.println(stackTop);
+			strFirst = inputStr.charAt(ip);
+//			String  vt = stack.peek();
+			if(stack.peek() == '#') {//弹出空串
+				stack.pop();
+			}else {
+				if(isVT(stack.peek())) {//栈顶是VT
+					if(stackTop == strFirst) {
+						stack.pop();
+						ip ++;
+					}else {//错误3 栈顶终结符与输入符不同
+						System.out.println("弹出栈顶终结符");
+						stack.pop();
+					}
+				}else {//栈顶是VN
+					String part = isInTable(stackTop,strFirst);
+					if(part!="false"){
+						stack.pop();
+						if(part != "synch") {
+							String rightPart = part.split("->")[1];
+							for(int i = rightPart.length()-1; i >= 0 ;i --) {//右部倒序入栈
+								stack.push(rightPart.charAt(i));
+							}
+							System.out.println(stack);
+							System.out.println(part);//输出产生式
+						}else {//错误2 查到synch
+							System.out.println("已弹出栈顶A");
+							stack.pop();
+						}	
+					}else {//错误1 查询分析表为空
+						System.out.println("跳过输入符号a");
+						ip ++;
+					}
+				}
+			}
+		}
+		System.out.println("完了！");
+	}
+	
+	public static void main(String[] args ){
+		// TODO Auto-generated method stub		
+		LL1Analyze LL1 = new LL1Analyze("i+i)$");
+//		System.out.println(LL1.stack.peek());
+		LL1.analyze();
+
+		
+	
+		}
+}
+ class PredictiveTable{
+	char[] VN = {'E','e','T','t','F','A','M'};
+	char[] VT = {'(',')','+','-','*','/','i','$'};
+	String[][] predictTable = {	{"E->Te" ,"synch",""      ,""      ,""      ,""      ,"E->Te","synch"},
+								{""      ,"e->#" ,"e->ATe","e->ATe",""      ,""      ,""     ,"e->#" },
+								{"T->Ft" ,"synch",""      ,""      ,""      ,""      ,"T->Ft","synch"},
+								{""      ,"t->#" ,"t->#"  ,"t->#"  ,"t->MFt","t->MFt",""     ,"t->#" },
+								{"F->(E)","synch",""      ,""      ,""      ,""      ,"F->i" ,"synch"},
+								{"synch" ,""     ,"A->+"  ,"A->-"  ,""      ,""      ,"synch",""     },
+								{"synch" ,""     ,""      ,""      ,"M->*"  ,"M->/"  ,"synch",""     }
+							};
+	
+}
